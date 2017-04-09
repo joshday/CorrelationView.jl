6d41bdcc73d199e50a82266fbbde2b9e843be57f
Josh Day <emailjoshday@gmail.com>
2017-04-09 12:07:47 -0500

    first commit

--- README.md
+++ README.md
@@ -5,3 +5,28 @@
 [![Coverage Status](https://coveralls.io/repos/joshday/CorrelationView.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/joshday/CorrelationView.jl?branch=master)
 
 [![codecov.io](http://codecov.io/github/joshday/CorrelationView.jl/coverage.svg?branch=master)](http://codecov.io/github/joshday/CorrelationView.jl?branch=master)
+
+
+Quick visualizations of correlation matrices
+
+```
+julia> using Distributions, CorrelationView
+
+julia> d = MvNormal(rand(10), [.7^abs(i-j) for i in 1:10, j in 1:10]);
+
+julia> y = rand(d, 1000);
+
+julia> c = cor(y');
+
+julia> corview(c)
+     71  52  38  28  14  12   9   6   3
+▓▓▓      71  52  34  16  12  10   8   3
+▒▒▒ ▓▓▓      72  45  28  19  15  11   7
+░░░ ▒▒▒ ▓▓▓      70  47  33  23  15  16
+░░░ ░░░ ▒▒▒ ▓▓▓      70  52  34  25  21
+        ░░░ ▒▒▒ ▓▓▓      70  46  34  28
+            ░░░ ▒▒▒ ▓▓▓      67  47  35
+            ░░░ ░░░ ▒▒▒ ▓▓▓      70  49
+                ░░░ ░░░ ▒▒▒ ▓▓▓      69
+                ░░░ ░░░ ░░░ ▒▒▒ ▓▓▓
+```
--- src/CorrelationView.jl
+++ src/CorrelationView.jl
@@ -1,5 +1,49 @@
 module CorrelationView
 
-# package code goes here
+export corview
+
+function corview(x::AbstractMatrix, io::IO = STDOUT)
+    issymmetric(x) || throw(ArgumentError("Matrix must be symmetric"))
+    p = size(x, 1)
+    for i in 1:p
+        for j in 1:p
+            y = x[i, j]
+            if j < i
+                print_element(io, y)
+            elseif i == j
+                print(io, "   ")
+            else
+                print_number(io, y)
+            end
+            print(io, " ")
+        end
+        println(io)
+    end
+end
+
+function intensity(y::Number)
+    ay = abs(y)
+    if ay > .8
+        return "███"
+    elseif ay > .6
+        return "▓▓▓"
+    elseif ay > .4
+        return "▒▒▒"
+    elseif ay > .2
+        return "░░░"
+    else
+        return "   "
+    end
+end
+
+function print_element(io::IO, y::Number)
+    color = ifelse(y > 0, :blue, :red)
+    print_with_color(color, io, intensity(y))
+end
+
+function print_number(io::IO, y::Number)
+    color = ifelse(y > 0, :blue, :red)
+    print_with_color(color, io, @sprintf("%3.0f", y * 100))
+end
 
 end # module
